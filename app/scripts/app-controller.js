d56a753dde9ed713ee616318f7978e7467c88caf
Tathaga7a <michael.s.kress@gmail.com>
2016-02-11 08:57:35 -0700

    first commit

---
+++ app/scripts/app-controller.js
@@ -0,0 +1,16 @@
+var PageCtrl = function(scope, location, http){
+	
+
+}
+
+(function(){
+	'use strict';
+	
+	angular
+		.module('appControllers', [])
+
+
+		.controller('PageCtrl', [PageCtrl]);
+	
+})();
+
---
+++ app/scripts/app.js
@@ -0,0 +1,45 @@
+var ready;
+ready = function () {
+	$('#mainnav')
+	.sidebar('attach events', '.menu.button', 'toggle');
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+(function(){
+	angular
+		.module('mkApp', [
+					'ngRoute', 
+					
+	])
+
+	
+		.config(['$routeProvider', function ($routeprovider) {
+			$routeprovider
+				.when ('/', {
+					templateUrl: 'partials/main.html', 
+					controller: 'PageCtrl'})
+				.when ('/climbing', {
+					templateUrl: 'partials/climbing.html', 
+					controller: 'PageCtrl'})
+				.when ('/work', {
+					templateUrl: 'partials/work.html', 
+					controller: 'PageCtrl'})
+				.when ('/media', {
+					templateUrl: 'partials/media.html', 
+					controller: 'PageCtrl'})
+				.when ('/contact', {
+					templateUrl: 'partials/contact.html', 
+					controller: 'PageCtrl'})
+				.otherwise ({
+					redirectTo: '/'});
+}]);
+
+})();
+
+
+
+
+
+
